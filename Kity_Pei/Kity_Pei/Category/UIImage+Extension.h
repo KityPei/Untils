6bc6fb4892b30c2230f9a0a099d972918617fd94
Kity_Pei <kity_pei@163.com>
2016-12-28 17:30:33 +0800

    commit

---
+++ Kity_Pei/Kity_Pei/Category/UIImage+Extension.h
@@ -0,0 +1,16 @@
+//
+//  UIImage+Extension.h
+//  Kity_Pei
+//
+//  Created by Kity_Pei on 2016/12/28.
+//  Copyright © 2016年 Kity_Pei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Extension)
+
+//  根据颜色创建图片
++ (UIImage *)imageWithColor:(UIColor *)color;
+
+@end
---
+++ Kity_Pei/Kity_Pei/Category/UIImage+Extension.m
@@ -0,0 +1,25 @@
+//
+//  UIImage+Extension.m
+//  Kity_Pei
+//
+//  Created by Kity_Pei on 2016/12/28.
+//  Copyright © 2016年 Kity_Pei. All rights reserved.
+//
+
+#import "UIImage+Extension.h"
+
+@implementation UIImage (Extension)
+
++ (UIImage *)imageWithColor:(UIColor *)color {
+    CGRect rect = CGRectMake(0.0f, 0.0f, 1.0f, 1.0f);
+    UIGraphicsBeginImageContext(rect.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextSetFillColorWithColor(context, [color CGColor]);
+    CGContextFillRect(context, rect);
+    UIImage *theImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return theImage;
+}
+
+@end
